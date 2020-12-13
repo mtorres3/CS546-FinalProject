26e7119cdbf5bff6cffd10952b8149df4e1b9480
Veronica Abramson <vabramson@users.noreply.github.com>
2020-12-13 15:12:42 -0500

    simple dash front-end

--- routes/trending.js
+++ routes/trending.js
@@ -5,7 +5,7 @@ const userData = data.users;
 const gameData = data.games;
 const reviewData = data.reviews;
 
-
+//need to sort games by most liked, then display limit 3 here
 router.get("/", async(request, response) => {
   try{
     response.render('extras/trending');
