d8865466394a0e1913349543ca5a1a6b0e82f88e
Veronica Abramson <vabramson@users.noreply.github.com>
2020-12-13 14:10:17 -0500

    added require commands for reviews and trending

---
+++ routes/trending.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const userData = data.users;
+
+router.get("/", async(request, response) => {
+  try{
+    response.render('extras/profile');
+  }
+  catch(e){
+    response.status(404).render('extras/error')
+  }
+});
+
+module.exports = router;
