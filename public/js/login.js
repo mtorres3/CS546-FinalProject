a381f817e1aefcfbeff6b3607e8eba98df86dfaa
Alejandro <abrancal@stevens.edu>
2020-12-15 16:55:30 -0500

    fixed logins

--- public/js/login.js
+++ public/js/login.js
@@ -18,31 +18,5 @@
       });
     });
 
-    loginForm.submit(function (event) {
-      event.preventDefault();
-      var user = $('#username'),
-      pw = $('#password');
-      console.log(user.val())
-      console.log(pw.val())
-      $.ajax({
-        url: "/profile/login",
-        type:"POST",
-        data: JSON.stringify({
-          username: user.val(),
-          password: pw.val()
-        }),
-        contentType: "application/json; charset=utf-8",
-        dataType   : "json",
-        success: function(data) {
-          console.log("login success")
-          divLogin.hide();
-          mainContent.show();
-        },
-        error: function() {
-          console.log('process error');
-        },
-      })
-
-    })
 
 })(window.jQuery);
